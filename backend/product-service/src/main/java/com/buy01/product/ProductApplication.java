341022671a88603ffef7bdb28c225dc1d3971524
rserraf <srrafedev@gmail.com>
2026-03-03 19:42:45 +0000

    fix: discovery for product and media services

--- backend/product-service/src/main/java/com/buy01/product/ProductApplication.java
+++ backend/product-service/src/main/java/com/buy01/product/ProductApplication.java
@@ -2,12 +2,14 @@ package com.buy01.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductApplication {
 
 	public static void main(String[] args) {
-		 String dbName = System.getenv("MONGO_DB_NAME");
+		 String dbName = System.getenv("PRODUCT_MONGO_DB_NAME");
         System.out.println("--- DEBUG: Database Name is: [" + dbName + "] ---");
 		SpringApplication.run(ProductApplication.class, args);
 	}

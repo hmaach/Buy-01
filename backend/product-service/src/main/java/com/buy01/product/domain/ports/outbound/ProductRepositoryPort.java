5a7eba8cdaf2355195122ad20f346783a67c6208
Hamza Maach <hamzamaach56@gmail.com>
2026-03-12 13:44:01 +0000

    feat: get products by userId

--- backend/product-service/src/main/java/com/buy01/product/domain/ports/outbound/ProductRepositoryPort.java
+++ backend/product-service/src/main/java/com/buy01/product/domain/ports/outbound/ProductRepositoryPort.java
@@ -8,9 +8,12 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ProductRepositoryPort {
+
     Mono<Product> save(Product product);
 
     Mono<Product> findById(String id);
+
+    Flux<Product> findByUserId(String userId);
 
     Mono<Void> deleteById(String id);
 

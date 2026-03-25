dc41b63d01bef759c76d135e1feade4e4e670cd4
rserraf <srrafedev@gmail.com>
2026-03-25 07:01:05 +0000

    wip: simple test for check jenkins

--- backend/product-service/src/test/java/com/buy01/product/ProductApplicationTests.java
+++ backend/product-service/src/test/java/com/buy01/product/ProductApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+// We use RANDOM_PORT to avoid "Port already in use" errors during Jenkins builds
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApplicationTests {
 
@@ -12,7 +13,13 @@ class ProductApplicationTests {
 	private WebTestClient webTestClient;
 
 	@Test
-	void testHealthCheck() {
+	void contextLoads() {
+		// If this method completes, the Spring Context (and WebFlux) started correctly
+	}
+
+	@Test
+	void checkHealthEndpoint() {
+		// This confirms your API is actually responding
 		webTestClient.get().uri("/actuator/health")
 				.exchange()
 				.expectStatus().isOk();

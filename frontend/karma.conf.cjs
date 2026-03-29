2632e07e7f27badd4c8817f379f1882fcf7ce3c2
rserraf <srrafedev@gmail.com>
2026-03-29 23:19:57 +0100

    wip: frontend test

--- frontend/karma.conf.cjs
+++ frontend/karma.conf.cjs
@@ -1,15 +1,14 @@
 const path = require('path');
-
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/build'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular/build/plugins/karma'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false,
